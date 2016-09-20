559b66a7fafe0e8ee07d9a6408dabf3be7d66074
volaya <volayaf@gmail.com>
2016-09-20 09:45:43 +0200

    add ext-libs folder when plugin is loaded

--- pluginname/__init__.py
+++ pluginname/__init__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import os
+import site
+
+site.addsitedir(os.path.abspath(os.path.dirname(__file__) + '/ext-libs'))
+
 def classFactory(iface):
     from plugin import [pluginclassname]
     return [pluginclassname](iface)
