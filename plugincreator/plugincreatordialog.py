ab91739b7f8693aa6411a0158c1fcfbcf76ce9e7
volaya <volayaf@gmail.com>
2018-11-23 09:22:15 +0100

    some ui fixes

--- plugincreator/plugincreatordialog.py
+++ plugincreator/plugincreatordialog.py
@@ -1,13 +1,32 @@
 import os
 from qgis.utils import iface
 from qgis.PyQt import uic
+from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtWidgets import QFileDialog
+from qgis.PyQt.QtWidgets import QSizePolicy
+from qgis.gui import QgsMessageBar
+from qgis.core import Qgis 
+
+from .selecttagsdialog import SelectTagsDialog
 
 pluginPath = os.path.dirname(__file__)
 WIDGET, BASE = uic.loadUiType(os.path.join(pluginPath, 'plugincreatordialog.ui'))
 
 class WrongTextValueException(Exception):
-    pass
+    
+    def __init__(self, widget, tab):
+        super(WrongTextValueException, self).__init__("")
+        self.widget = widget
+        self.tab = tab
+        self.errormessage = "Wrong or missing parameter value"
+
+class WrongNumValueException(Exception):
+
+    def __init__(self, widget, tab):
+        super(WrongNumValueException, self).__init__("")
+        self.widget = widget
+        self.tab = tab
+        self.errormessage = "Version numbers must be numeric"
 
 
 class PluginCreatorDialog(BASE, WIDGET):
@@ -16,37 +35,70 @@ class PluginCreatorDialog(BASE, WIDGET):
         parent = parent or iface.mainWindow()
         super(PluginCreatorDialog, self).__init__(parent)
         self.setupUi(self)
+        self.bar = QgsMessageBar()
+        self.bar.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+        self.layout().insertWidget(0, self.bar)
         self.pluginInfo = None
         self.outputFolder = None
         self.btnFolder.clicked.connect(self.selectFolder)
+        self.btnTags.clicked.connect(self.selectTags)
         self.buttonBox.accepted.connect(self.okPressed)
         self.buttonBox.rejected.connect(self.cancelPressed)
+        self.chkQgisCommons.stateChanged.connect(self.qgiscommonsChanged)
+
+    def qgiscommonsChanged(self, state):
+        state = state == Qt.Checked
+        self.chkSettings.setEnabled(state)
+        self.chkHelp.setEnabled(state)
+        self.chkAbout.setEnabled(state)
+
+    def selectTags(self):
+        dialog = SelectTagsDialog()
+        dialog.show()
+        ok = dialog.exec_()
+        if ok:
+            selected = tag_dialog.listView.selectedIndexes()
+            seltags = []
+            for tag in selected:
+                seltags.append(tag.data())
+            taglist = ", ".join(seltags)
+            self.dialog.tags.setText(taglist)
 
     def selectFolder(self):
         folder = QFileDialog.getExistingDirectory(self, 'Select output folder')
         if folder:
             self.txtFolder.setText(folder)
 
-    def _getTextValue(self, widget):
-        return widget.text()
+    def _getTextValue(self, widget, tab):
+        text = widget.text().strip()
+        if text == "":
+            raise WrongTextValueException(widget, tab)
+        widget.setStyleSheet("QLineEdit{background: white}")
+        return text
 
-    def _getNumValue(self, widget):
+    def _getNumValue(self, widget, tab):
+        value = self._getTextValue(widget, tab)
+        try:
+            num = float(value)
+        except:
+            raise WrongNumValueException(widget, tab)
+        widget.setStyleSheet("QLineEdit{background: white}")
         return widget.text()
 
     def okPressed(self):
         self.pluginInfo = {}
         try:
-            self.pluginInfo["pluginName"] = self._getTextValue(self.txtName) or "My New Plugin"
-            self.pluginInfo["description"] = self._getTextValue(self.txtDescription) or "bla"
-            self.pluginInfo["about"] = self.txtAbout.toPlainText() or "blabla"
-            self.pluginInfo["author"] = self._getTextValue(self.txtAuthor) or "victor"
-            self.pluginInfo["email"] = self._getTextValue(self.txtEmail) or "volaya@bla.com"
-            self.pluginInfo["minVersion"] = self._getNumValue(self.txtQgisVersion)
-            self.pluginInfo["version"] = self._getNumValue(self.txtVersion)
-            self.pluginInfo["homepage"] = self._getTextValue(self.txtHomepage)
-            self.pluginInfo["tracker"] = self._getTextValue(self.txtTracker)
-            self.pluginInfo["repository"] = self._getTextValue(self.txtRepository)
-            self.pluginInfo["tags"] = self._getTextValue(self.txtTags)
+            self.pluginInfo["pluginName"] = self._getTextValue(self.txtName, 0)
+            self.pluginInfo["description"] = self._getTextValue(self.txtDescription, 0)
+            self.pluginInfo["about"] = self.txtAbout.toPlainText()
+            self.pluginInfo["author"] = self._getTextValue(self.txtAuthor, 0)
+            self.pluginInfo["email"] = self._getTextValue(self.txtEmail, 0)
+            self.pluginInfo["minVersion"] = self._getNumValue(self.txtQgisVersion, 0)
+            self.pluginInfo["version"] = self._getNumValue(self.txtVersion, 0)
+            self.pluginInfo["homepage"] = self.txtHomepage.text()
+            self.pluginInfo["tracker"] = self._getTextValue(self.txtTracker, 2)
+            self.pluginInfo["repository"] = self._getTextValue(self.txtRepository, 2)
+            self.pluginInfo["tags"] = self.txtTags.text()
             self.pluginInfo["experimental"] = self.chkExperimental.isChecked()
             self.pluginInfo["addTests"] = self.chkTests.isChecked()
             self.pluginInfo["addLessons"] = self.chkLessons.isChecked()
@@ -54,15 +106,24 @@ class PluginCreatorDialog(BASE, WIDGET):
             self.pluginInfo["addDocs"] = self.chkDocs.isChecked()
             qgiscommons = self.chkQgisCommons.isChecked()
             self.pluginInfo["addQgisCommons"] = qgiscommons
-            if qgiscommons:
-                self.pluginInfo["addSettings"] = self.chkSettings.isChecked()
-                self.pluginInfo["addAbout"] = self.chkAbout.isChecked()
-                self.pluginInfo["addHelp"] = self.chkHelp.isChecked()
+            self.pluginInfo["addSettings"] = self.chkSettings.isChecked() and qgiscommons
+            self.pluginInfo["addAbout"] = self.chkAbout.isChecked() and qgiscommons
+            self.pluginInfo["addHelp"] = self.chkHelp.isChecked() and qgiscommons
         except WrongTextValueException as e:
             self.pluginInfo = None
+            self._showError(e)
+            return
+        except WrongTextValueException as e:
+            self.pluginInfo = None
+            self._showError(e)
             return
         self.outputFolder = self.txtFolder.text()
         self.close()
+
+    def _showError(self, e):
+        e.widget.setStyleSheet("QLineEdit{background: yellow}")
+        self.tabWidget.setCurrentIndex(e.tab)
+        self.bar.pushMessage("", e.errormessage, level=Qgis.Warning, duration=5)
 
     def cancelPressed(self):
         self.pluginInfo = None
---
+++ plugincreator/selecttagsdialog.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+
+import os
+
+from qgis.PyQt import QtGui, QtWidgets, uic
+
+WIDGET, BASE = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'selecttagsdialog.ui'))
+
+
+class SelectTagsDialog(QtWidgets.QDialog, WIDGET):
+    def __init__(self, parent=None):
+        super(SelectTagsDialog, self).__init__(parent)
+        self.setupUi(self)
+
+        tagFile = os.path.join(os.path.dirname(__file__), 'taglist.txt')
+
+        with open(tagFile) as tf:
+            tags = tf.readlines()
+
+        model = QtGui.QStandardItemModel()
+
+        for tag in tags:
+            item = QtGui.QStandardItem(tag[:-1])
+            model.appendRow(item)
+
+        self.listView.setModel(model)
